85bf60f6c7388e1b36ce50b2a33bd937cb5fc48e
Ironam1 <ironam1@icloud.com>
2019-09-20 11:18:58 -0400

    added navbar

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./style.css";
+import NavBar from "./NavBar";
 
 function Header() {
-
-    return (
+  return (
     <div className="jumbotron jumbotron-fluid">
-        <div className="container">
+      <div className="container">
         <h1 className="display-4">Catchy Site Name</h1>
         <p className="lead">Tracking Baby's Basic Needs</p>
-        </div>
+      </div>
+      <NavBar></NavBar>
     </div>
-    )
+  );
 }
 export default Header;
