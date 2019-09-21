1b8c5954e45def201c88b81c48b0d3c290cec2f8
ScrantonPete <scrantonpete@yahoo.com>
2019-09-20 22:20:16 -0400

    added DatePicker to change.js page

--- client/src/pages/change.js
+++ client/src/pages/change.js
@@ -1,46 +1,50 @@
 import React, { Component } from "react";
 import "./style.css";
-
-import 'rc-time-picker/assets/index.css';
+import DatePicker from "../components/DatePicker";
+import "rc-time-picker/assets/index.css";
 // import ReactDom from 'react-dom';
-import moment from 'moment';
-import TimePicker from 'rc-time-picker';
+import moment from "moment";
+import TimePicker from "rc-time-picker";
 
-const format = 'h:mm a';
-const now = moment().hour(0).minute(0);
+const format = "h:mm a";
+const now = moment()
+  .hour(0)
+  .minute(0);
 
 class Change extends Component {
-    state = {
-    };
+  state = {};
 
-    // onChange = value => {
-    // console.log(value && value.format(format))
-    // }
+  // onChange = value => {
+  // console.log(value && value.format(format))
+  // }
 
-    render () {
-        return (
-        <div className="container">
-            <h2>Change Me</h2>
-
-            <TimePicker
-                showSecond={false}
-                defaultValue={now}
-                className="xxx"
-                // onChange={onChange}
-                format={format}
-                use12Hours
-                inputReadOnly
-            />,
-
-            <div class="input-group">
-                <div class="input-group-prepend">
-                <label class="input-group-text">Details</label>
-                </div>
-                <textarea class="form-control"></textarea>
-            </div>
-            <button type="button" class="btn btn-info" id="save">Save</button>
+  render() {
+    return (
+      <div className="container">
+        <DatePicker></DatePicker>
+        <h2>Change Me</h2>
+        <TimePicker
+          showSecond={false}
+          defaultValue={now}
+          className="xxx"
+          // onChange={onChange}
+          format={format}
+          use12Hours
+          inputReadOnly
+        />
+        ,
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <label class="input-group-text">Details</label>
+          </div>
+          <textarea class="form-control"></textarea>
         </div>
-         ) };
+        <button type="button" class="btn btn-info" id="save">
+          Save
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Change;
