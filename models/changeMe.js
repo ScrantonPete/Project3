c20b28fd897179a87946fa657b2461c3e8a3273b
Ironam1 <ironam1@icloud.com>
2019-09-26 11:38:13 -0400

    api route working with new model and controller structure

--- models/changeMe.js
+++ models/changeMe.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const changeMeSchema = new Schema({
   user: {type: String, required: true },
   date: { type: String, required: true },
-  time: { type: String, required: true },
+  time: { type: String, required: true, unique: true },
   details: String
 });
 
