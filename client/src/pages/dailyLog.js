b2567b8b50ce0fef8b14777153d06e376342b21e
conniepope <conniepope77@gmail.com>
2019-09-30 19:55:24 -0400

    posts & gets working and displaying data

--- client/src/pages/dailyLog.js
+++ client/src/pages/dailyLog.js
@@ -1,36 +1,58 @@
 import React, { Component } from "react";
 import "./style.css";
-import Container from "../components/container";
+import Container from "../components/Container/container";
 import API from "../utils/API";
 
-
 class DailyLog extends Component {
-  state = {};
+  state = {
+    feed: [],
+    sleep: [],
+    change: []
+  }
 
-  componentDidMount = () => {
+  componentDidMount = (feed, sleep, change) => {
     console.log("mounted");
+    console.log(this.state.change)
 
-    API.getFeed()
-      .then(res => this.setState({ feed: res.data }))
+    API.getFeed(feed)
+      .then(res => {
+        this.setState({ feed: res.data })
+        console.log(res.data)
+      })
       .catch(err => console.log("Error" + err));
 
-    API.getSleep()
-      .then(res => this.setState({ sleep: res.data }))
+    API.getSleep(sleep)
+      .then(res => {
+        this.setState({ sleep: res.data })
+      })
       .catch(err => console.log("Error" + err));
 
-    API.getChange()
-      .then(res => this.setState({ change: res.data }))
+    API.getChange(change)
+      .then(res => {
+        this.setState({ change: res.data })
+      })
       .catch(err => console.log("Error" + err));
+
   };
 
   render() {
-    return (
-      <div className="container">
+
+      return (
+  
+        <div className="container">
         <h2>Daily Log</h2>
         <div className="log">
-          <Container>
-          <p>Sample text</p>
-
+          <Container
+            itemList={this.state.change}
+            title="Changes"> 
+          </Container>
+          <Container
+            itemList={this.state.sleep}
+            title="Sleep"> 
+          </Container>
+          <Container
+            itemList={this.state.feed}
+            title="Feed"> 
           </Container>
         </div>
       </div>
